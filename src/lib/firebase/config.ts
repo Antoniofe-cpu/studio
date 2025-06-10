97fb3be4145a7f0ff8cbff9e2d5ca0e956754805
Antonio Ferrara <ferrara.antonio08@gmail.com>
2025-06-10 10:05:50 +0000

    main

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,13 +5,13 @@ import { getAuth, type Auth } from 'firebase/auth';
 
 // Construct Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBsup1LANccRepTNF0Y1CoBcfMjnqZGkyw",
+  authDomain: "watchgraph-a91fd.firebaseapp.com",
+  projectId: "watchgraph-a91fd",
+  storageBucket: "watchgraph-a91fd.firebasestorage.app",
+  messagingSenderId: "984650741137",
+  appId: "1:984650741137:web:861fec7023fe4afbe5d630",
+  measurementId: "G-8KZVYL1TGD",
 };
 
 // Validate that all required Firebase config values are present
